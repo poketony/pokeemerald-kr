a0cfcc768e9fa652f720ff6b148d931baea45f18
koipkoi <hrh6914@gmail.com>
2022-10-03 23:14:00 +0900

    폰트 변경 (#15)
    1, 2번 폰트를 7번과 기본 교체

--- gflib/text.c
+++ gflib/text.c
@@ -1674,10 +1674,12 @@ void DecompressGlyphFont7(u16 glyphId, bool32 isJapanese)
         if (glyphId >= 0x3700)
         {
             u16 index = glyphId - 0x3700;
-            glyphs = gFont7KoreanGlyphs + (0x20 * index);
+            glyphs = gFont2KoreanGlyphs + (0x20 * index);
             DecompressGlyphTile(glyphs, gUnknown_03002F90.unk0);
+            DecompressGlyphTile(glyphs + 0x8, gUnknown_03002F90.unk20);
             DecompressGlyphTile(glyphs + 0x10, gUnknown_03002F90.unk40);
-            gUnknown_03002F90.width = 8;
+            DecompressGlyphTile(glyphs + 0x18, gUnknown_03002F90.unk60);
+            gUnknown_03002F90.width = 11;
             gUnknown_03002F90.height = 15;
         }
         else
@@ -1706,7 +1708,7 @@ void DecompressGlyphFont7(u16 glyphId, bool32 isJapanese)
 u32 GetGlyphWidthFont7(u16 glyphId, bool32 isJapanese)
 {
     if (IsKoreanGlyph(glyphId))
-        return 8;
+        return 11;
     if (isJapanese == TRUE)
         return 8;
     return gFont7LatinGlyphWidths[glyphId];
@@ -1788,12 +1790,10 @@ void DecompressGlyphFont2(u16 glyphId, bool32 isJapanese)
         if (glyphId >= 0x3700)
         {
             u16 index = glyphId - 0x3700;
-            glyphs = gFont2KoreanGlyphs + (0x20 * index);
+            glyphs = gFont7KoreanGlyphs + (0x20 * index);
             DecompressGlyphTile(glyphs, gUnknown_03002F90.unk0);
-            DecompressGlyphTile(glyphs + 0x8, gUnknown_03002F90.unk20);
             DecompressGlyphTile(glyphs + 0x10, gUnknown_03002F90.unk40);
-            DecompressGlyphTile(glyphs + 0x18, gUnknown_03002F90.unk60);
-            gUnknown_03002F90.width = 11;
+            gUnknown_03002F90.width = 8;
             gUnknown_03002F90.height = 14;
         }
         else
@@ -1822,7 +1822,7 @@ void DecompressGlyphFont2(u16 glyphId, bool32 isJapanese)
 u32 GetGlyphWidthFont2(u16 glyphId, bool32 isJapanese)
 {
     if (IsKoreanGlyph(glyphId))
-        return 11;
+        return 8;
     if (isJapanese == TRUE)
         return gFont2JapaneseGlyphWidths[glyphId];
     return gFont2LatinGlyphWidths[glyphId];
@@ -1846,12 +1846,10 @@ void DecompressGlyphFont1(u16 glyphId, bool32 isJapanese)
         if (glyphId >= 0x3700)
         {
             u16 index = glyphId - 0x3700;
-            glyphs = gFont2KoreanGlyphs + (0x20 * index);
+            glyphs = gFont7KoreanGlyphs + (0x20 * index);
             DecompressGlyphTile(glyphs, gUnknown_03002F90.unk0);
-            DecompressGlyphTile(glyphs + 0x8, gUnknown_03002F90.unk20);
             DecompressGlyphTile(glyphs + 0x10, gUnknown_03002F90.unk40);
-            DecompressGlyphTile(glyphs + 0x18, gUnknown_03002F90.unk60);
-            gUnknown_03002F90.width = 11;
+            gUnknown_03002F90.width = 8;
             gUnknown_03002F90.height = 15;
         }
         else
@@ -1880,7 +1878,7 @@ void DecompressGlyphFont1(u16 glyphId, bool32 isJapanese)
 u32 GetGlyphWidthFont1(u16 glyphId, bool32 isJapanese)
 {
     if (IsKoreanGlyph(glyphId))
-        return 11;
+        return 8;
     if (isJapanese == TRUE)
         return 8;
     return gFont1LatinGlyphWidths[glyphId];
